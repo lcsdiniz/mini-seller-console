f26ce8e41dea177bdaa8c67921f08f95d30b512d
lcsdiniz <lucasdiniz.smt@gmail.com>
2025-09-14 10:44:42 -0300

    feat: reducing paddding in mobile

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
       <Topbar />
       <div className="flex h-screen bg-gray-100">
         <div className="flex-1 flex flex-col">
-          <main className="flex-1 overflow-y-auto p-6">
+          <main className="flex-1 overflow-y-auto p-6 sm:p-4 xs:p-2">
             <AppRoutes />
           </main>
         </div>
